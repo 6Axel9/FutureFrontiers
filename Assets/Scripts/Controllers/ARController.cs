33edafc9962ed0ad9ccadedbe935f9b07eae7e13
Erik di Biase <oberonx10@gmail.com>
2021-11-19 10:45:05 +0100

    Created ARController
    Added AR spawnable prefabs
    Added ARController functionalities
    Corrected Image Tracking list and Scene setting for testing

--- Assets/Scripts/Controllers/ARController.cs
+++ Assets/Scripts/Controllers/ARController.cs
@@ -1,18 +1,150 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class ARController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const string CoreImageTrackable = "Syncer";
+
+    [SerializeField]
+    private ARSession session;
+    [SerializeField]
+    private ARSessionOrigin origin;
+    [SerializeField]
+    private Transform content;
+
+    private ARAnchorManager anchorManager;
+    private ARRaycastManager raycastManager;
+    private ARTrackedImageManager imageManager;
+
+    public bool IsContentAnchored => content.parent != null;
+    public ARSessionState State => ARSession.state;
+
+    private bool hasAnchor;
+    private int elapsedFrames;
+    private int maxFrames = 30;
+
+    private void Awake()
     {
-        
+        Application.targetFrameRate = maxFrames;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        anchorManager = origin.GetComponent<ARAnchorManager>();
+        raycastManager = origin.GetComponent<ARRaycastManager>();
+        imageManager = origin.GetComponent<ARTrackedImageManager>();
+
+        anchorManager.anchorsChanged += OnAnchorsChanged;
+        imageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    public void EnableImageTracking()
+    {
+        imageManager.enabled = true;
+
+        Debug.Log("ImageTracking Enabled");
+    }
+
+    public void DisableImageTracking()
+    {
+        imageManager.enabled = false;
+        elapsedFrames = 0;
+
+        Debug.Log("ImageTracking Disabled");
+    }
+
+    public void EnableAR()
+    {
+        session.gameObject.SetActive(true);
+    }
+
+    public void DisableAR()
+    {
+        session.gameObject.SetActive(false);
+    }
+
+    private void OnAnchorsChanged(ARAnchorsChangedEventArgs anchorsChangedArgs)
+    {
+        foreach (ARAnchor anchor in anchorsChangedArgs.added)
+        {
+            Debug.Log("Anchor Added");
+
+            AnchorContent(anchor);
+        }
+    }
+
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs trackedImagesChangedArgs)
+    {
+        foreach (ARTrackedImage image in trackedImagesChangedArgs.updated)
+        {
+            if (image.referenceImage.name == CoreImageTrackable)
+                SyncContent(image);
+        }
+    }
+
+    private void SyncContent(ARTrackable syncer)
+    {
+        Debug.Log("Content Placed");
+
+        content.rotation = syncer.transform.rotation;
+        content.position = syncer.transform.position;
+        elapsedFrames++;
+
+        if (IsContentAnchored && elapsedFrames > maxFrames)
+            DisableImageTracking();
+
+        if (hasAnchor)
+            return;
+
+        RaycastTo(syncer.transform, AddAnchor);
+        hasAnchor = true;
+    }
+
+    private void AnchorContent(ARAnchor newAnchor)
+    {
+        if (IsContentAnchored)
+            RemoveAnchor();
+
+        content.SetParent(newAnchor.transform);
+
+        Debug.Log("Content Activeted");
+
+        content.gameObject.SetActive(true);
+    }
+
+    private void AddAnchor(ARRaycastHit hit)
+    {
+        Debug.Log("Anchor Created");
+
+        anchorManager.AttachAnchor(hit.trackable as ARPlane, hit.pose);
+    }
+
+    private void RemoveAnchor()
+    {
+        Debug.Log("Anchor Removed");
+
+        GameObject oldAnchor = content.parent.gameObject;
+        content.SetParent(null);
+        Destroy(oldAnchor);
+    }
+
+    public void RaycastTo(Transform target, Action<ARRaycastHit> callback = null)
+    {
+        List<ARRaycastHit> results = new List<ARRaycastHit>();
+        Vector3 direction = target.position - origin.camera.transform.position;
+        Ray ray = origin.camera.ScreenPointToRay(origin.camera.ViewportToScreenPoint(new Vector2(0.5f, 0.5f)));
+
+        Debug.Log("Raycast Peformed");
+
+        if (!raycastManager.Raycast(ray, results, TrackableType.PlaneWithinBounds))
+            return;
+
+        Debug.Log("Raycast Successful");
+
+        callback?.Invoke(results.OrderBy(hit => hit.pose.position.y).First());
     }
 }
